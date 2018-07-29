7bc5fa1fed001c776cbd23b4e227b6f69f529279
hjain0726 <hjain0726@gmail.com>
2018-07-29 22:09:36 +0530

    first commit

---
+++ utils/httpskeys.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+const pathConfig = require("./config");
+
+const httpOptions = {
+  cert: fs.readFileSync(path.join(pathConfig.HTTPSBASEPATH, "/cert.pem")),
+  key: fs.readFileSync(path.join(pathConfig.HTTPSBASEPATH, "/key.pem"))
+}
+
+module.exports = httpOptions;
